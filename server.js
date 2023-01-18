bd7b22115ace57771603e8401c988f24f513bcd9
Garjan44 <grgosain@gmail.com>
2023-01-18 16:09:17 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Garjan Raj Gosain  Student ID: 134210210 Date: 2023/01/18
+*
+*  Online (Cyclic) URL:  
+*
+********************************************************************************/ 
+
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+const path=require("path")
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Garjan Raj Gosain - 134210210");
 });
+
+app.get("/views", function(req,res){
+    req.sendFile(path.join(__dirname,"/views.about.html"));
+})
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
